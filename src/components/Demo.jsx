73e841c98f5e1d23c978c9532c065a90a064b1c7
Daniel Ukaji <dannyukaji@gmail.com>
2023-04-30 21:41:32 +0100

    New Commit

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -48,7 +48,7 @@ const Demo = () => {
   }
 
   return (
-    <section className="mt-16 w-full max-w-xl">
+    <section className="mt-16 w-full max-w-xl overflow-x-hidden">
       {/* Search */}
       <div className="flex flex-col w-full gap-2">
         <form
@@ -58,7 +58,7 @@ const Demo = () => {
             <img 
               src={linkIcon}
               alt="link_icon"
-              className="absolute left-0 my-2 ml-3 w-5"
+              className="absolute xl:left-0 left-7 my-2 ml-3 w-5"
             />
 
             <input 
@@ -79,7 +79,7 @@ const Demo = () => {
         </form>
 
         {/* Browser URL History */}
-        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+        <div className="flex flex-col gap-1 max-h-60 justify-center xl:justify-start items-center xl:items-start overflow-y-auto">
           {allArticles.map((item, index) => (
             <div 
               key={`link-${index}`} 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import { logo } from '../assets';
 
 const Hero = () => {
   return (
-    <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+    <header className='w-full flex justify-center items-center flex-col overflow-x-hidden'>
+      <nav className='flex justify-between items-center w-10/12 xl:w-full mb-10 pt-3'>
         <img 
           src={logo} 
           alt='sumz_logo' 
@@ -18,10 +18,10 @@ const Hero = () => {
         </button>
       </nav>
 
-      <h1 className='head_text'>Summarize Articles with <br className='max-md:hidden' />
+      <h1 className='head_text w-10/12 xl:w-full'>Summarize Articles with <br className='max-md:hidden' />
         <span className='orange_gradient'>OpenAI GPT-4</span>
       </h1>
-      <h2 className='desc'>
+      <h2 className='desc w-10/12 xl:w-full'>
         Simplify your reading with Summize, an open-source article
         summarizer that transforms lengthy articles into clear and
         concise summaries
